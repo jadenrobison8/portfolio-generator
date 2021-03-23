96387cca12cbb9f3051d39cf19cdf719fd506b69
robisonja <jadenrobison8@gmail.com>
2021-03-22 21:01:39 -0600

    HTML to handle more data

--- app.js
+++ app.js
@@ -1,16 +1,8 @@
 const { prototype } = require('events');
 const inquirer = require('inquirer');
-//const fs = require('fs');
-//const generatePage = require('./src/page-template.js');
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
-//const pageHTML = generatePage(Name, github);
-
-
-//fs.writeFile('index.html', generatePage(Name, github), err => {
-//    if (err) throw err;
-
-//    console.log('Porfolio complete! Check out index.html to see the output!')
-//});
 
 const promptUser = () =>  {
     return inquirer.prompt([
@@ -144,6 +136,12 @@ const promptProject = portfolioData => {
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        console.log(portfolioData);
+        const pageHTML = generatePage(portfolioData);
+
+        fs.writeFile('index.html', pageHTML, err => {
+            if (err) throw err;
+
+            console.log('Porfolio complete! Check out index.html to see the output!')
+        });
     });
     
